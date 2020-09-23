0a422160adf1fefd5471e7853ad82a8e89726bf4
kamilatrhea <k.wojnowski@rheagroup.com>
2020-09-23 12:59:27 +0200

    test project added

---
+++ DEHPCommon.Tests/CommonUserInterface/CommonUserInterfaceTestFixture.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CommonUserInterfaceTestFixture.cs"company="RHEA System S.A.">
+//    Copyright(c) 2020 RHEA System S.A.
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Kamil Wojnowski, Nathanael Smiechowski.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DEHPCommon.Tests.CommonUserInterface
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="CommonUserInterfaceTestFixture"/> class.
+    /// </summary>
+    [TestFixture]
+    public class CommonUserInterfaceTestFixture
+    {
+        private string serverType;
+        private string uri;
+        private string userName;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            this.serverType = "CDP4 WebServicesNew";
+            this.uri = "http://localhost:4000";
+            this.userName = "DEHP-UserNew";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void Verify_that_on_write_ArgumentNullException_is_thrown()
+        {
+            //Assert.Throws<ArgumentNullException>(() => this.userPreferenceService.Write<UserPreference>(null));
+        }
+    }
+}
