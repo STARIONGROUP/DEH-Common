73e77301c520d629982eae7a1f018db746a3808a
kamilatrhea <k.wojnowski@rheagroup.com>
2020-09-24 11:56:51 +0200

    MessageHub class added

---
+++ DEHPCommon/MessageHub/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MessageHub.cs"company="RHEA System S.A.">
+//    Copyright(c) 2020 RHEA System S.A.
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Kamil Wojnowski, Nathanael Smiechowski.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DEHPCommon.MessageHub
+{
+    /// <summary>
+    /// Definition of the <see cref="MessageHub"/>
+    /// </summary>
+    public class MessageHub
+    {
+    }
+}
